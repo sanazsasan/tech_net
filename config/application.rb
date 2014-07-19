ad1b947822344935b1b8996c619858a5f1a031dc
sanaz <mhedayati1357@gmail.com>
2014-07-19 15:45:30 -0400

    set config.secret_key

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module TechNet
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.secret_key_base = 'fa3b4086388713d1180099a7a382a31e9bf6cd6d944ec4318dbe842cca55ed748b57633e2c082eddc54080fd3e058e0331f0535a1b21ad65b81eb5cf43a5e490'
   end
 end
 
